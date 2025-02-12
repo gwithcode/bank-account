6059b2f78cb0c5da66372ad61947a9463a0c9f57
Zach Shimberg <shimbergz@gmail.com>
2025-02-12 18:06:36 -0500

    added monthy_interest

--- savings.py
+++ savings.py
@@ -3,5 +3,8 @@ from bank import BankAccount
 class savings(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
-        self.interest = 0.04
-        
+        self.interest_rate = 0.04
+
+    def monthy_interest(self):
+        interest = self.current_balance * self.interest_rate
+        self.current_balance += interest
