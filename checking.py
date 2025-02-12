f1d32d5a7386f0dc7fde888ce49a5a415b6bacc6
Daniel <dmezasar@charlotte.edu>
2025-02-12 18:12:28 -0500

    Transfer method added

--- checking.py
+++ checking.py
@@ -4,4 +4,17 @@ from bank import BankAccount
 class CheckingAccount(BankAccount):
 
     def __init__(self, customer_name, current_balance, minimum_balance):
-        
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
+        self.limit = current_balance / 4
+
+    def transfer(self, amount):
+
+        if amount <= 0:
+            print("Error: amount must be positive and not 0")
+        elif amount > self.limit:
+            print("Error: amount exceeds transfer limit")
+        elif amount > self.current_balance:
+            print("Error: insufficient funds")
+        else:
+            self.current_balance -= amount
+            print(f"Transfer successful: ${amount} transferred.")
