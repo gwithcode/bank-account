1de8a2a732eac3eb2be34beb98aa0451667a21a3
Zach Shimberg <shimbergz@gmail.com>
2025-02-12 18:15:05 -0500

    updated monthy_interest to properly caculate interest based on mounth.

--- savings.py
+++ savings.py
@@ -3,8 +3,9 @@ from bank import BankAccount
 class savings(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
-        self.interest_rate = 0.04
+        self.annual_interest_rate = 0.04
 
     def monthy_interest(self):
-        interest = self.current_balance * self.interest_rate
+        monthy_rate = self.annual_interest_rate/12
+        interest = self.current_balance * monthy_rate
         self.current_balance += interest
