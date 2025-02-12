747a0ed49980f505630ff2a2000082e61deb906e
Daniel <dmezasar@charlotte.edu>
2025-02-12 18:12:12 -0500

    Formated

--- checking.py
+++ checking.py
@@ -1,5 +1,7 @@
 from bank import BankAccount
 
-class checkingAccount(BankAccount):
+
+class CheckingAccount(BankAccount):
+
     def __init__(self, customer_name, current_balance, minimum_balance):
         
